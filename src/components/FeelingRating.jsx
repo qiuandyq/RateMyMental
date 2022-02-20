5adc624780c086a4f96add7be2d8fe4140de4c25
dlilly99 <drewraylilly@gmail.com>
2022-02-19 17:27:33 -0800

    reSubmit
    made the buttons go away and come back after a new button is pressed

--- src/components/FeelingRating.jsx
+++ src/components/FeelingRating.jsx
@@ -4,40 +4,58 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 
 import { db } from "./firebase";
-import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { doc, updateDoc, arrayUnion} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
+import React,{useState} from 'react';
 
 const FeelingRating = () => {
 
   const auth = getAuth();
   const [user] = useAuthState(auth);
-
+  const[show,setShow]=useState(true);
 
   const sendRating = async (event) => {
     
     var userRef = doc(db, "users", user.uid);
     try{
       await updateDoc(userRef, {
-          ratings: arrayUnion({
-            rating: event.target.value,
+          ratings: arrayUnion({ 
+            rating: event.currentTarget.value,
             date: new Date().toString()
           }),
+          
       });
+      setShow(false);
+      
+
     }
     catch(err){
       console.log(err);
     }
 
   };
+  const reSubmit = async () =>{
+    try{
+      setShow(true);
+    }
+    catch(err){
+      console.log(err)
+    }
+  };
 
   return (
+    
+    <div >
+      {show?
+
     <Grid
       container
       direction="column"
       justifyContent="center"
       alignItems="center"
     >
+    
       <Grid item xs={12}>
         <Typography component={"span"}>
           <Box sx={{ textAlign: "center" }} mt={10}>
@@ -191,7 +209,35 @@ const FeelingRating = () => {
           </Grid>
         </Box>
       </Grid>
+      
     </Grid>
+    :<Grid container direction="column" justifyContent="center" alignItems="center">
+        <Grid item xs={12}>
+          <Typography component={"span"}>
+            <Box sx={{ textAlign: "center" }} mt={10}>
+              {" "}
+              <div style={{ fontSize: "50px" }}>Check-in sent</div>{" "}
+            </Box>
+          </Typography>
+        </Grid>
+        <Grid item xs={12} mt={3}>
+          <Button 
+              onClick={ () => reSubmit() }
+              style={{
+                borderWidth: "2.8px",
+                minWidth: "90px",
+                minHeight: "90px",
+                textAlign:"center",
+              }}
+              variant="outlined"
+            >
+              <div style={{ fontSize: "30px" }}>Submit another rating</div>
+            </Button>
+        </Grid>
+      
+    </Grid>
+  }</div>
+    
   );
 };
 
